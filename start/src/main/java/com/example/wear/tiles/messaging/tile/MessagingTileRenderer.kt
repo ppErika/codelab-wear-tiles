0ffd1b9faa1afee5a7e6ffeb29ee482beda540c2
Erika_Han <hanerika11@gmail.com>
2024-03-18 10:13:38 +0900

    미리보기 함수 추가

--- start/src/main/java/com/example/wear/tiles/messaging/tile/MessagingTileRenderer.kt
+++ start/src/main/java/com/example/wear/tiles/messaging/tile/MessagingTileRenderer.kt
@@ -17,8 +17,10 @@ package com.example.wear.tiles.messaging.tile
 
 import android.content.Context
 import android.graphics.Bitmap
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.wear.tiles.ColorBuilders
 import androidx.wear.tiles.DeviceParametersBuilders
 import androidx.wear.tiles.LayoutElementBuilders
@@ -28,6 +30,9 @@ import androidx.wear.tiles.material.Typography
 import androidx.wear.tiles.material.layouts.PrimaryLayout
 import com.example.wear.tiles.R
 import com.example.wear.tiles.messaging.Contact
+import com.example.wear.tiles.messaging.MessagingRepo
+import com.example.wear.tiles.tools.WearDevicePreview
+import com.google.android.horologist.compose.tools.TileLayoutPreview
 import com.google.android.horologist.tiles.images.drawableResToImageResource
 import com.google.android.horologist.tiles.images.toImageResource
 import com.google.android.horologist.tiles.render.SingleTileLayoutRenderer
@@ -82,3 +87,14 @@ private fun messagingTileLayout(
             .build()
     )
     .build()
+
+
+@WearDevicePreview
+@Composable
+fun MessagingTileRendererPreview() {
+    TileLayoutPreview(
+        state = MessagingTileState(MessagingRepo.knownContacts),
+        resourceState = emptyMap(),
+        renderer = MessagingTileRenderer(LocalContext.current)
+    )
+}
